397d947ab3592759069fb03e3357d5f1a75cb019
SmaragdaRp <smararep@gmail.com>
2021-09-22 12:46:37 +0300

    22.09 push backend

--- back-end/linkedit_project/linkedit_app/serializers.py
+++ back-end/linkedit_project/linkedit_app/serializers.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from .models import *
 
 
-# 29-8 - DONE
+#######################################          USER          ########################################
+
 class SignUpSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -20,10 +21,10 @@ class SignUpSerializer(serializers.ModelSerializer):
         if password is not None:
             instance.set_password(password)
         instance.save()
-        return Response(instance.email)
+        return Response(instance)           ## instance.email
 
 
-
+# POST personal_data    -   UNDER TESTING
 class PersonalDataSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -31,23 +32,52 @@ class PersonalDataSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'content', 'visibility']
 
     def create(self, validated_data):
-        personal_data_instance, created = PersonalData.objects.get_or_create(validated_data)
-
         user = self.context.get('user')
+        title = validated_data.pop('title')
+
+        user_existance = MyUser.objects.get(id=user.id)
+
+        if user_existance.personal_data.filter(title__contains=title).exists():
+            pdata = user_existance.personal_data.get(title__contains=title)
+            user_existance.personal_data.remove(pdata)
+
+        personal_data_instance = PersonalData.objects.create(title=title,
+                                                             content=validated_data.pop('content'),
+                                                             visibility=validated_data.pop('visibility'))
         user.personal_data.add(personal_data_instance)
         user.save()
         return(personal_data_instance)
 
 
+# POST user/  UserAuthView  -   DONE
 class UserAuthSerializer(serializers.ModelSerializer):
+    # avatar = serializers.SerializerMethodField('get_avatar_url')
     personal_data = PersonalDataSerializer(many=True)
 
     class Meta:
         model = MyUser
         fields = ['id', 'username', 'avatar', 'first_name', 'last_name', 'phone', 'email', 'personal_data']
 
+    # def get_avatar_url(self, obj):
+    #     # request = self.context.get('request')
+    #     # photo_url = obj.fingerprint.url
+    #     return request.build_absolute_uri(photo_url)
 
 
+class SimpleUserSerializer(serializers.ModelSerializer):
+    # avatar = serializers.SerializerMethodField('get_avatar_url')
+
+    class Meta:
+        model = MyUser
+        fields = ['id', 'avatar', 'first_name', 'last_name']
+
+    # def get_avatar_url(self, obj):
+    #     request = self.context.get('request')
+    #     photo_url = obj.fingerprint.url
+    #     return request.build_absolute_uri(photo_url)
+
+
+# POST settings/user_data   -   DONE
 class ChangeUserSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -55,35 +85,229 @@ class ChangeUserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'avatar', 'first_name', 'last_name', 'phone', 'email', 'personal_data']
 
 
+# POST settings/user_data   -   DONE
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    new = serializers.CharField(source='password')
 
-# # 1-9
-# class ContentSerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = Content
-#         fields = ['text', 'file']
-#
-#
-# class LikeSerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = LikeInteraction
-#         fields = ['like']
-#
-#
-# class CommentSerializer(serializers.ModelSerializer):
-#
-#     class Meta:
-#         model = Comment
-#         fields = ['commenter', 'text']
-#
-#
-# class AllPostsSerializer(serializers.ModelSerializer):
-#     content = ContentSerializer(read_only=True, many=True)
-#     likes = LikeSerializer(read_only=True, many=True)
-#     comments = CommentSerializer(read_only=True, many=True)
-#     # poster = serializers.
-#
-#     class Meta:
-#         model = Post
-#         fields = ['poster', 'date', 'content', 'likes', 'comments']
+    class Meta:
+        model = MyUser
+        fields = ['id', 'new']
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data.get('password'))
+        instance.save()
+        return instance
+
+
+# POST settings/user_data   -   DONE
+class ProfileImageSerializer(serializers.ModelSerializer):
+    # image_url = serializers.SerializerMethodField('get_url')
+    class Meta:
+        model = MyUser
+        fields = ['id', 'avatar']
+
+    def update(self, instance, validated_data):
+        image = self.context.get('image')
+        instance.avatar = image
+        instance.save()
+        return instance
+
+
+######################################         FRIENDS         #######################################
+
+
+class NotAFriendSerializer(serializers.ModelSerializer):
+    connected = serializers.SerializerMethodField()
+    personal_data = PersonalDataSerializer(many=True)
+
+    class Meta:
+        model = MyUser
+        fields = ['id', 'connected', 'username', 'avatar', 'first_name', 'last_name', 'phone', 'email', 'personal_data']
+
+    def get_connected(self, obj):
+        return False
+
+
+class FriendSerializer(serializers.ModelSerializer):
+    connected = serializers.SerializerMethodField()
+    personal_data = PersonalDataSerializer(many=True)
+
+    class Meta:
+        model = MyUser
+        fields = ['id', 'connected','username', 'avatar', 'first_name', 'last_name', 'phone', 'email', 'personal_data']
+
+    def get_connected(self, obj):
+        return True
+
+
+class FriendShipSerializer(serializers.ModelSerializer):
+    friends = FriendSerializer(many=True)
+
+    class Meta:
+        model = FriendshipList
+        fields = ['friends']
+
+
+class FriendRequestSerializer(serializers.ModelSerializer):
+    # connected = SerializerMethodField('exists_in_list')
+    # personal_data = PersonalDataSerializer(many=True)
+    sender = NotAFriendSerializer(read_only=False)
+
+    class Meta:
+        model = FriendRequest
+        fields = ['id', 'sender']
+
+
+##################################        POSTS AND PROPOSALS        ###################################
+
+class TheFileField(serializers.Field):
+
+    def to_representation(self, value):
+        ret = {
+            "type": value.type,
+            "url": value.url.url
+        }
+        return ret
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    file = TheFileField(source='*')
+
+    class Meta:
+        model = Content
+        fields = ['text', 'file']
+
+    # def get_url_url(self, obj):
+    #     request = self.context.get('request')
+    #     photo_url = obj.fingerprint.url
+    #     return request.build_absolute_uri(photo_url)
+
+
+class TextContentField(serializers.Field):
+
+    def to_representation(self, value):
+        ret = {
+            "text": value.text
+        }
+        return ret
+
+    def to_internal_value(self, data):
+        ret = {'content': {
+            'text': data['text']
+        }}
+        return ret
+
+
+class CommentInteraction(serializers.ModelSerializer):
+    user = FriendSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['user']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = SimpleUserSerializer(read_only=False)
+    content = TextContentField(source='*')
+
+    class Meta:
+        model = Comment
+        fields = ['user', 'content']
+
+    def create(self, validated_data):
+        text = self.context.get('text')
+        comment_instance = Comment.objects.create(text=text, user=self.context.get('user'))
+        post = self.context.get('post')
+        post.comments.add(comment_instance)
+        post.save()
+
+        return comment_instance
+
+
+class PostCommentInteraction(serializers.ModelSerializer):
+    comments = CommentInteraction(many=True)
+
+    class Meta:
+        model = Post
+        fields = ['comments']
+
+
+class PostLikeInteraction(serializers.ModelSerializer):
+    likes = FriendSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Post
+        fields = ['likes']
+
+
+# GET recommended or my posts   -
+class PostsSerializer(serializers.ModelSerializer):
+    content = ContentSerializer()
+    likes = FriendSerializer(many=True)
+    comments = CommentSerializer(many=True)
+    user = SimpleUserSerializer(read_only=False)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'date', 'user', 'content', 'likes', 'comments']
+
+    # def get_url_url(self, obj):
+    #     request = self.context.get('request')
+    #     photo_url = obj.fingerprint.url
+    #     return request.build_absolute_uri(photo_url)
+
+
+class ProposalLikeInteraction(serializers.ModelSerializer):
+    likes = FriendSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = JobProposal
+        fields = ['likes']
+
+
+# GET recommended or my job proposals   -
+class ProposalsSerializer(serializers.ModelSerializer):
+    content = ContentSerializer()
+    likes = FriendSerializer(many=True)
+    user = SimpleUserSerializer(read_only=False)
+
+    class Meta:
+        model = JobProposal
+        fields = ['id', 'date', 'user', 'content', 'likes']
+
+
+class InteractionsSerializer(serializers.Serializer):
+    likes = PostLikeInteraction(read_only=True, many=True)
+    apply = ProposalLikeInteraction(read_only=True, many=True)
+    comments = PostCommentInteraction(read_only=True, many=True)
+
+
+#######################################          CHAT          ########################################
+
+class MessageSerializer(serializers.ModelSerializer):
+    user = SimpleUserSerializer(required=False, read_only=False)
+    content = TextContentField(source='*')
+
+    class Meta:
+        model = Message
+        fields = ['user', 'content']
+
+    def create(self, validated_data):
+        text = self.context.get('text')
+        message_instance = Message.objects.create(text=text, user=self.context.get('user'))
+
+        chat = self.context.get('chat')
+        chat.messages.add(message_instance)
+        chat.save()
+
+        return message_instance
+
+
+class ChatSerializer(serializers.ModelSerializer):
+    user1 = FriendSerializer(read_only=True)
+    user2 = FriendSerializer(read_only=True)
+    messages = MessageSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Chat
+        fields = ['id', 'user1', 'user2', 'messages']
--- back-end/linkedit_project/linkedit_app/urls.py
+++ back-end/linkedit_project/linkedit_app/urls.py
@@ -1,17 +1,43 @@
 from django.urls import path
 from .views import *
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 
 # router = DefaultRouter()
 # router.register(r'settings/user_data', ChangeUserView, basename='settings/user_data/')
 
 urlpatterns = [
-    path('sign_up/', SignUpView.as_view(), name='sign_up'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('user/', UserAuthView.as_view(), name='user'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('settings/user_data/', ChangeUserView.as_view(), name='user_data'),
-    path('personal_data/', ChangePersonalDataView.as_view(), name='personal_data'),
-    # path('posts/recommended', AllPostsView.as_view(), name='all_posts')
+    #####################################          USER          ######################################
+    path('sign_up', SignUpView.as_view(), name='sign_up'),
+    path('login', LoginView.as_view(), name='login'),
+    path('user', LoginView.as_view(), name='user'),
+    path('logout', LogoutView.as_view(), name='logout'),
+    path('settings/user_data', ChangeUserDataView.as_view(), name='user_data'),
+    path('personal_data', ChangePersonalDataView.as_view(), name='personal_data'),
+    path('settings/profile_image', PostProfileImage.as_view(), name='profile_image'),
+    path('settings/change_password', ChangePasswordView.as_view(), name='change_password'),
+    path('login/forgot_password', ForgotPasswordView.as_view(), name='forgot_password'),
+    path('adminsite/users', GetAllUsersView.as_view(), name='all_users'),
+    path('search_results', SearchUserView.as_view(), name='search_user'),
+
+    ####################################         FRIENDS         #####################################
+    path('notifications/requests', FriendRequestView.as_view(), name='friend_requests'),
+    path('notifications/requests/respond', FriendRequestView.as_view(), name='respond_to_friend_request'),
+    path('connected_users', GetFriendsView.as_view(), name='users_friends'),
+
+    ###############################        POSTS AND PROPOSALS        ################################
+    path('posts/recommended/', PostsView.as_view(), name='all_posts'),
+    path('posts/upload', PostsView.as_view(), name='post_post'),
+    path('posts/my/', MyPostsView.as_view(), name='my_posts'),
+    path('proposals/apply', ThumbsUpView.as_view(), name='like_post_proposal'),
+    path('posts/comment', PostCommentView.as_view(), name='post_comment'),
+    path('proposals/recommended', ProposalsView.as_view(), name='all_proposals'),
+    path('proposals/upload', ProposalsView.as_view(), name='post_proposal'),
+    path('proposals/my', MyProposalsView.as_view(), name='my_proposals'),
+    path('notifications/interactions', UserInteractionsView.as_view(), name='interactions'),
+
+    #####################################          CHAT          ######################################
+    path('discussions', GetChatsView.as_view(), name='get_chats'),
+    path('discussions/id', GetChatfromIdView.as_view(), name='get_a_chat'),
+    path('discussions/message', SendMessageView.as_view(), name='send_message'),
+
 ]
-              # + router.urls
